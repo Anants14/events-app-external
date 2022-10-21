7a42f213ff60745e3fc112e98efdeae992495141
Anant <anantsaini14@gmail.com>
2022-10-21 08:51:27 +0000

    updated application spell

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Gelato Application</h1>"));
+         chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Gelato application</h1>"));
         return done();
       });
 
